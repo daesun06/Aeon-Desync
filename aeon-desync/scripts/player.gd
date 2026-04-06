8c45102ad4c0dfeda9c90c55ab2dd8a6c7e125f4
adhd-ev <151930850+adhd-ev@users.noreply.github.com>
2026-04-06 08:32:25 +0500

    refactor player.gd

--- aeon-desync/scripts/player.gd
+++ aeon-desync/scripts/player.gd
@@ -1,64 +1,33 @@
 extends CharacterBody2D
-
-
-const SPEED = 100.0
+const SPEED = 120.0
 var current_direction = "none"
 
-func _ready():
-	$AnimatedSprite2D.play("idle_down")
+func _ready():$AnimatedSprite2D.play("idle_down") 
 
 func _physics_process(delta):
 	player_movement(delta)
 	
-func player_movement(delta):
-	if Input.is_action_pressed("ui_left"):
+func player_movement(uidelta):
+	if Input.is_action_pressed("left"):
 		current_direction = "left"
 		play_animation(1)
-		velocity.x = -SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_right"):
+		velocity = Vector2(-SPEED,0)
+	elif Input.is_action_pressed("right"):
 		current_direction = "right"
 		play_animation(1)
-		velocity.x = SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
+		velocity=Vector2(SPEED,0)
+	elif Input.is_action_pressed("down"):
 		current_direction = "down"
 		play_animation(1)
-		velocity.x = 0
-		velocity.y = SPEED
-	elif Input.is_action_pressed("ui_up"):
+		velocity=Vector2(0,SPEED)
+	elif Input.is_action_pressed("up"):
 		current_direction = "up"
 		play_animation(1)
-		velocity.x = 0
-		velocity.y = -SPEED
+		velocity=Vector2(0,-SPEED)
 	else:
 		play_animation(0)
-		velocity.x = 0
-		velocity.y = 0
-		
+		velocity = Vector2.ZERO
 	move_and_slide()
 
 func play_animation(movement):
-	var dir = current_direction
-	var anim = $AnimatedSprite2D
-	
-	if dir == "right":
-		if movement == 1:
-			anim.play("walk_right")
-		else:
-			anim.play("idle_right")
-	if dir == "left":
-		if movement == 1:
-			anim.play("walk_left")
-		else:
-			anim.play("idle_left")
-	if dir == "up":
-		if movement == 1:
-			anim.play("walk_up")
-		else:
-			anim.play("idle_up")
-	if dir == "down":
-		if movement == 1:
-			anim.play("walk_down")
-		else:
-			anim.play("idle_down")
+	$AnimatedSprite2D.play(StringName("%s_%s"%["walk" if movement else "idle",current_direction]))
